46bb2f3ca6755035dada387e588c91806ba95dc1
Loguntsov Sergey <loguntsov@gmail.com>
2012-11-23 16:02:16 +0400

    Генерация pid-файла при запуске.

--- src/erlycounter_app.erl
+++ src/erlycounter_app.erl
@@ -33,6 +33,10 @@ init(Options) ->
 	{port_range, Port_range} = proplists:lookup(port_range, Options), % {port_from, port_to}
 	{save_process, Save_process} = proplists:lookup(save_process, Options), %integer
 	{save_time, Save_time} = proplists:lookup(save_time, Options), %integer
+	{pid_file, Pid_file} = proplists:lookup(pid_file, Options), %binary
+
+	file:write_file(Pid_file, os:getpid()),
+
 	{ ok, {{one_for_one, 5, 1000}, [
 		{ecsaver_pool, { ecsaver, pool_start, [ Save_process ] }, permanent, 5000 , supervisor, [] },
 		{ectable_sup, { ectable_sup, start_link, [ server_id, Tables, Save_time*1000, Command ] }, permanent, 5000 , supervisor, [] },
